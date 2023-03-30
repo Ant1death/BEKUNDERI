146e941af836ed80468cad943e0c3a1b8907e1b8
Ant1death <Ant1death@mail.ru>
2023-03-30 16:16:50 +0300

    fix-1

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,48 @@
-console.log('da');
+'use strict'
+window.addEventListener('DOMContentLoaded',() => {
+    // настройка инпутов
+    const inputs = document.querySelectorAll('input');
+    if(inputs) {
+        inputs.forEach((item,i) => {
+            if(item.value.length > 0) {
+                item.classList.add('focusedInput');
+            }
+            item.addEventListener('input',() => {
+                if(item.value.length > 0) {
+                    item.classList.add('focusedInput');
+                } else {
+                    item.classList.remove('focusedInput');
+                }
+            });
+        });
+    }
+    // переключение в редакторе шаблона карт
+    const sampleWalletBtns = document.querySelectorAll('.sample-wallets__btn');
+    const sampleWalletWrappers = document.querySelectorAll('.sample__wrapper_wallet');
+
+    function hideTabs() {
+        sampleWalletWrappers.forEach(item => {
+            item.classList.remove('active');
+        });
+
+        sampleWalletBtns.forEach(item => {
+            item.classList.remove('active');
+        });
+    }
+    function showTabs(i = 0) {
+        sampleWalletWrappers[i].classList.add('active');
+        sampleWalletBtns[i].classList.add('active');
+    }
+    window.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('sample-wallets__btn')) {
+            sampleWalletBtns.forEach((item, i) => {
+                if (target == item) {
+                    hideTabs();
+                    showTabs(i);
+                }
+            });
+        }
+    });
+});
