3c540d4a2ccfd345a59238ded26c01b628842b03
Ant1death <Ant1death@mail.ru>
2023-03-30 23:18:10 +0300

    settings

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 'use strict'
 window.addEventListener('DOMContentLoaded',() => {
     // настройка инпутов
-    const inputs = document.querySelectorAll('input');
+    const inputs = document.querySelectorAll('input[type="text"]');
     if(inputs) {
         inputs.forEach((item,i) => {
             if(item.value.length > 0) {
@@ -45,4 +45,56 @@ window.addEventListener('DOMContentLoaded',() => {
             });
         }
     });
+    // modalki
+    const modals = document.querySelectorAll('.modal');
+    const modalBtns = document.querySelectorAll('.general-item__btn');
+    const modalExitBtns = document.querySelectorAll('.modal-exit');
+
+    
+    if(modalBtns) {
+        modalBtns.forEach((btn,i) => {
+            btn.addEventListener('click', (e) => {
+                modals.forEach((modal,j) => {
+                    if(btn.id.slice(0, -3) === modal.id.slice(0, -5)) {
+                        modal.classList.add('active');
+                    }
+                });
+            });
+        });
+        modalExitBtns.forEach((item,i) => {
+            item.addEventListener('click',(e) => {
+                modals.forEach((modal,j) => {
+                    modal.classList.remove('active');
+                });
+            });
+        });
+        modals.forEach((modal,i) => {
+            modal.addEventListener('click',(e) => {
+                if(e.target === modal) {
+                    modal.classList.remove('active');
+                }
+            });
+        })
+    }
+
+    
+
+    // window.addEventListener('click',(e) => {
+    //     const target = e.target;
+
+    //     if(target.id.slice(0, -3)) {
+    //         modalBtns.forEach((btn,i) => {
+    //             modals.forEach((modal,j) => {
+    //                 if(btn.id.slice(0, -3) === modal.id.slice(0, -5)) {
+    //                     modal.classList.add('active');
+    //                 }
+    //             });
+    //         });
+    //     }
+    //     if(target.classList.contains('modal__header-exit') || target.classList.contains('modal__footer-btn_exit') || target.classList.contains('modal active')) {
+    //         console.log(target);
+    //     }
+        
+        
+    // });
 });
