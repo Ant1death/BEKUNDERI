684a887ec50a023b5c50cef20ec09798e481324b
Ant1death <Ant1death@mail.ru>
2023-04-20 13:57:33 +0300

    fix-1

--- src/js/modules/sw2.js
+++ src/js/modules/sw2.js
@@ -134,7 +134,7 @@ function sw2() {
               console.log(formData);
               axios({
                 method: 'post',
-                url: '/settings-accesses',
+                url: '/settings/accesses',
                 headers: {}, 
                 data: formData
               }).then((response) => {
